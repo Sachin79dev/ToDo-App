4834a62c161ec1a281aed9768eba8c9f2d189fa4
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-13 18:20:17 +0530

    Refactor task management with edit and delete functionality
    Refactor task management with edit and delete functionality

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let inp = document.querySelector('#task');
 let addBtn = document.querySelector('#add');
 let taskList = document.querySelector("#taskBox");
-let edit = document.querySelector('#edit_task');
+
+
+let editId = null;
+
+let id = 0;
 
 
 
-addBtn.addEventListener('click', (e) => {
+let addTodo = addBtn.addEventListener('click', (e) => {
     e.preventDefault();
     let task = inp.value
 
@@ -14,15 +18,54 @@ addBtn.addEventListener('click', (e) => {
     }
 
 
+    if (editId) {
+        editId.textContent = inp.value;
+        editId = null;
+        inp.value = '';
+        return;
+    }
+
+
+
+    // let ids = document.querySelector('.ids');
+
+    // ids.setAttribute('id', id)
+
+    id++
+
     taskList.innerHTML += `
-            <div class="li">
-                <p>${task}</p>
+            <div class="li" id="${id}">
+                <p class="ids">${task}</p>
                 <div class="d-flex g-2">
-                    <button class="btn btn-primary" id="edit_task">Edit</button>
-                    <button class="btn btn-danger" id="delete_task">Delete</button>
+                    <button class="btn btn-primary edit_task">Edit</button>
+                    <button class="btn btn-danger delete_task">Delete</button>
                 </div>
             </div>`
-
     inp.value = ''
+
+    let del = document.querySelectorAll('.delete_task')
+
+    del.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            btn.closest('.li').remove()
+        })
+    })
+
+
+    let edit = document.querySelectorAll('.edit_task');
+
+    edit.forEach((upt) => {
+        upt.addEventListener('click', () => {
+            let taskItem = upt.closest('.li');
+            let taskText = taskItem.querySelector('.ids');
+
+            inp.value = taskText.textContent;
+
+            editId = taskText;
+
+            console.log(editId);
+            
+        })
+    })
 
 })
